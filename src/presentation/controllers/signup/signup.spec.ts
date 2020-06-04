7dc1f6c275a86f7e3d357f9f2b60dd4874e8713c
Artur Paes <arturopaes10@gmail.com>
2020-06-04 11:54:20 -0300

    refactor: move HttpHelepr to a folder

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -7,7 +7,7 @@ import {
   Validation
 } from './signup-protocols'
 import { HttpRequest } from '../../protocols'
-import { ok, serverError, badRequest } from '../../helpers/http-helper'
+import { ok, serverError, badRequest } from '../../helpers/http/http-helper'
 
 const makeFakeAccount = (): AccountModel => ({
   id: 'valid_id',
