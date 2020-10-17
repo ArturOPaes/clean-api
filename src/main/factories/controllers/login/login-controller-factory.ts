d99631f622d7123c3309d2a6d9e2d9825e098f6e
Artur Paes <arturopaes@gmail.com>
2020-10-17 19:57:54 -0300

    refactor: organize main layer folders

--- src/main/factories/controllers/login/login-controller-factory.ts
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -2,7 +2,7 @@ import { LoginController } from '../../../../presentation/controllers/authentica
 import { Controller } from '../../../../presentation/protocols'
 import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
 import { makeLoginValidation } from '../login/login-validation-factory'
-import { makeDbAuthentication } from '../usecases/db-authentication-factory'
+import { makeDbAuthentication } from '../usecases/account/db-authentication-factory'
 
 export const makeLoginController = (): Controller => {
   return makeLogControllerDecorator(
